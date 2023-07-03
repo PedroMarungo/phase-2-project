dd72cd69247db31372f994d948721a880b5c3665
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 20:59:33 -0500

    set useEffect with fetch to local restful api

--- src/components/BlogLists.jsx
+++ src/components/BlogLists.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 function BlogLists() {
   const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/blogs")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
   return <div>BlogLists</div>;
 }
 
