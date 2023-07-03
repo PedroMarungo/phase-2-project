14b6d4b93fb6e6d570e7e421d30174118a31e16a
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 20:43:17 -0500

    added nav container for links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <Router>
+      <nav></nav>
       <div>
         <Routes>
           <Route path="/" element={<BlogList />} />
