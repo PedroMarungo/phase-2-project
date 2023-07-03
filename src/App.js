b8b62d125b2621260d3514c5fe2995ff8f783337
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 20:39:59 -0500

    added routes inside router (bloglist/createblog)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import CreateBlog from "./components/CreateBlog";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BlogList />} />
+        <Route path="/create" element={<CreateBlog />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
