8afbfc679f52e4fb82d66c50327be94421bf238b
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 21:13:00 -0500

    created state for title, content, author, & init  useNavigate

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function CreateBlog() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const navigate = useNavigate();
   return <div>CreateBlog</div>;
 }
 
