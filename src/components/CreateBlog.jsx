e43cd993026a78a4341e139cef90b17c28f99cbb
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 21:14:16 -0500

    create handleSubmit event & fetch local restful data with POST/GET

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -5,6 +5,26 @@ function CreateBlog() {
   const [content, setContent] = useState("");
   const [author, setAuthor] = useState("");
   const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBlog = { title, author, content };
+
+    fetch("http://localhost:3000/blogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBlog),
+    })
+      .then(() => {
+        setTitle("");
+        setAuthor("");
+        setContent("");
+        navigate("/");
+      })
+      .catch((error) => console.error("Error:", error));
+  };
   return <div>CreateBlog</div>;
 }
 
