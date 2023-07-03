b16d4606eae94ce0b02ff6081df2c4bcd95f387c
Pedro Marungo <pedromarungo@gmail.com>
2023-07-02 20:37:47 -0500

    imported boostrap to app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import BlogList from "./components/BlogList";
 import CreateBlog from "./components/CreateBlog";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
